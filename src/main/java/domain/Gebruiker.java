902ec5ae8d4e9c56054a575bf6fe4a80c9a79c4b
ThijsVries <t.vries@outlook.com>
2018-02-23 10:42:20 +0100

    Further implementation of domain model

--- src/main/java/domain/Gebruiker.java
+++ src/main/java/domain/Gebruiker.java
@@ -1,13 +1,135 @@
 package domain;
 
+import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Gebruiker {
+public class Gebruiker implements Serializable{
     
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String profilePicture;
+    private String password;
+    
+    private String bio;
+    private String website;
+    private String location;
+    
+    @OneToMany
+    private List<Gebruiker> following;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Gebruiker> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(List<Gebruiker> following) {
+        this.following = following;
+    }
+     
+    public Gebruiker(){
+        
+    }
+    
+    public Gebruiker(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public Gebruiker(long Id, String firstName, String lastName, String email, String profilePicture, String password, String bio, String website, String location) {
+        this.Id = Id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.profilePicture = profilePicture;
+        this.password = password;
+        this.bio = bio;
+        this.website = website;
+        this.location = location;
+    }
+    
+    public void follow(Gebruiker gebruiker){
+        
+    }
+    
+    public void unFollow(Gebruiker gebruiker){
+        
+    }
+    
+    
+    
+    
+    
+    
 }
--- src/main/java/domain/Kweet.java
+++ src/main/java/domain/Kweet.java
@@ -1,14 +1,104 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package domain;
 
-/**
- *
- * @author Tvrie
- */
-public class Kweet {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Kweet implements Serializable{
+    
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long Id;
+    private String message;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+    private List<String> tags = new ArrayList();
+    @ManyToOne
+    private Gebruiker ownedBy;
+    @OneToMany
+    private List<Gebruiker> likes;
+    @OneToMany
+    private List<Gebruiker> mentioned;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Gebruiker getOwnedBy() {
+        return ownedBy;
+    }
+
+    public void setOwnedBy(Gebruiker ownedBy) {
+        this.ownedBy = ownedBy;
+    }
+
+    public List<Gebruiker> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Gebruiker> likes) {
+        this.likes = likes;
+    }
+
+    public List<Gebruiker> getMentioned() {
+        return mentioned;
+    }
+
+    public void setMentioned(List<Gebruiker> mentioned) {
+        this.mentioned = mentioned;
+    }
+    
+    public Kweet(){
+        
+    }
+    
+    public Kweet(Gebruiker gebruiker, String message){
+        this.ownedBy = gebruiker;
+        this.message = message;
+    }
+    
+    /**
+     * Likes the selected kweet.
+     * @param gebruiker The user liking the tweet
+     */
+    public void like(Gebruiker gebruiker){
+        if(!likes.contains(gebruiker)){
+            likes.add(gebruiker);
+        }
+    }
+    
+    
+    
+    
     
 }
