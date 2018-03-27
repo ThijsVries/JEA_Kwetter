a5038ed60c53e615ea4ceecb4fba7a4ca171356e
ThijsVries <t.vries@outlook.com>
2018-03-27 17:01:30 +0200

    Removed specifed version on maven plugin

--- target/generated-sources/annotations/domain/Kweet_.java
+++ target/generated-sources/annotations/domain/Kweet_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T16:56:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T17:00:59")
 @StaticMetamodel(Kweet.class)
 public class Kweet_ { 
 
